e6b8c0134d967d53f4192e39c979c1417992cdd2
nullrequest <30698906+Lunarequest@users.noreply.github.com>
2021-09-01 12:10:21 +0530

    fixed gpg signing

--- start-github-desktop.sh
+++ start-github-desktop.sh
@@ -2,4 +2,6 @@
 
 export TMPDIR=$XDG_RUNTIME_DIR/app/$FLATPAK_ID
 export FLATPAK_HOST=1 
+gpg-agent --homedir ~/.gnupg --daemon --pinentry-program=/app/bin/pinentry
 exec zypak-wrapper /app/github-desktop/github-desktop "$@"
+pkill -TERM gpg-agent
