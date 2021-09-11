8a4cfbba1094e4dfc8286667b1e3e57946528996
Luna <advaith.madhukar@gmail.com>
2021-09-11 18:03:49 +0530

    force use of gio portals for trash

--- start-github-desktop.sh
+++ start-github-desktop.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 export TMPDIR=$XDG_RUNTIME_DIR/app/$FLATPAK_ID
 export FLATPAK_HOST=1 
+export ELECTRON_TRASH=gio
 gpg-agent --homedir ~/.gnupg --daemon --pinentry-program=/app/bin/pinentry
 exec zypak-wrapper /app/github-desktop/github-desktop "$@"||exit 0
 pkill -TERM gpg-agent
