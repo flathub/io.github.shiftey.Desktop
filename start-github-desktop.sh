0f423b514f6be97c571ec0635b0b7951d2be42c5
Luna <advaith.madhukar@gmail.com>
2021-09-25 08:20:06 +0530

    drop pinentry switch to fdo runtime

--- start-github-desktop.sh
+++ start-github-desktop.sh
@@ -2,6 +2,4 @@
 export TMPDIR=$XDG_RUNTIME_DIR/app/$FLATPAK_ID
 export FLATPAK_HOST=1 
 export ELECTRON_TRASH=gio
-gpg-agent --homedir ~/.gnupg --daemon --pinentry-program=/app/bin/pinentry
-exec zypak-wrapper /app/github-desktop/github-desktop "$@"||exit 0
-pkill -TERM gpg-agent
+exec zypak-wrapper /app/github-desktop/github-desktop "$@"
